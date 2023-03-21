51862c5fd8dd0b5b539d068ce01a2cb0b653ce8e
Gnana Ganesh <gnana.ganesh@thoughtworks.com>
2023-03-21 17:27:13 +0530

    [Ganesh] Add. Tic tac toe source code

--- rust_exercise/src/main.rs
+++ rust_exercise/src/main.rs
@@ -1,72 +1,147 @@
-extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
-extern crate tokio_core;
-extern crate futures;
-extern crate hyper;
-extern crate hyper_tls;
-extern crate twitter_api;
-extern crate oauth_client as oauth;
+extern crate termcolor;
 
-use std::time;
-use std::env;
-use std::collections::HashMap;
-use std::path::Path;
-use std::path::PathBuf;
+use std::io::Write;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
-mod config;
-mod crawler;
-mod twitter;
+fn greeting() {
+    println!(
+        "\nRust TicTacToe\n\
+         --------------\n\
+         A simple game written in the rust programming language.\n\
+         Code is available at: https://github.com/flofriday/tictactoe"
+    )
+}
 
-const CONF_FILENAME: &'static str = ".crypto-bot.conf";
+fn print_player(player: &char) {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
-fn get_home_dir() -> PathBuf {
-    match env::home_dir() {
-        Some(p) => p,
-        None => {
-            panic!("Impossible to get your home dir!");
+    if player == &'X' {
+        stdout
+            .set_color(ColorSpec::new().set_fg(Some(Color::Blue)))
+            .unwrap();
+    } else if player == &'O' {
+        stdout
+            .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
+            .unwrap();
+    }
+
+    write!(&mut stdout, "{}", player).unwrap();
+    stdout.reset().unwrap();
+}
+
+fn draw(state: &[char]) {
+    println!("\n");
+
+    for i in (0..3).rev() {
+        let offset = i * 3;
+
+        print!("-------------\n| ");
+        print_player(&state[offset]);
+        print!(" | ");
+        print_player(&state[offset + 1]);
+        print!(" | ");
+        print_player(&state[offset + 2]);
+        println!(" |");
+    }
+
+    println!("-------------");
+}
+
+fn ask_user(state: &mut [char], player: char) {
+    loop {
+        print!("Player '");
+        print_player(&player);
+        println!("', enter a number: ");
+
+        let mut input = String::new();
+        if std::io::stdin().read_line(&mut input).is_err() {
+            println!("Couldn't read line! Try again.");
+            continue;
+        }
+
+        if let Ok(number) = input.trim().parse::<usize>() {
+            if number < 1 || number > 9 {
+                println!("The field number must be between 1 and 9.");
+                continue;
+            }
+
+            let number = number - 1;
+
+            if state[number] == 'X' || state[number] == 'O' {
+                print!("This field is already taken by '");
+                print_player(&state[number]);
+                println!("'.");
+                continue;
+            }
+
+            state[number] = player;
+
+            break;
+        } else {
+            println!("Only numbers are allowed.");
+            continue;
         }
     }
 }
 
-pub fn build_message (prices : &mut HashMap<&str, crawler::CurrencyPrice>) -> String {
-    let mut s = String::from("");
-    for (cur, value) in prices.iter() {
-        let t = format!("#{}: ${} ${}€\n",cur, value.USD, value.EUR);
-        s = s + &t[..];
+fn has_won(state: &[char]) -> bool {
+    for tmp in 0..3 {
+        if state[tmp] == state[tmp + 3] && state[tmp] == state[tmp + 6] {
+            return true;
+        }
+
+        let tmp = tmp * 3;
+
+        if state[tmp] == state[tmp + 1] && state[tmp] == state[tmp + 2] {
+            return true;
+        }
     }
 
-    return s;
+    if (state[0] == state[4] && state[0] == state[8])
+        || (state[2] == state[4] && state[2] == state[6])
+    {
+        return true;
+    }
+
+    false
+}
+
+#[inline(always)]
+fn is_over(state: &[char]) -> bool {
+    state.iter().all(|&v| v == 'X' || v == 'O')
 }
 
 fn main() {
-    let mut conf_file_path: PathBuf = get_home_dir();
-    conf_file_path.push(Path::new(CONF_FILENAME));
+    let mut state = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let mut player = 'X';
 
-
-    let config = match config::Config::read(&conf_file_path) {
-        Some(v) => v,
-        None => panic!("Cannot find config file"),
-    };
-
-    let crawler = crawler::Crawler::new();
-    let twitter = twitter::Twitter::new(config.consumer_key, config.consumer_secret,
-                                        config.access_key, config.access_secret);
-
-    //let mut prices : HashMap<&str,crawler::CurrencyPrice>= HashMap::new();
-    let mut prices = HashMap::new();
+    // Welcome the player
+    greeting();
 
     loop {
-        for currency in &config.currencies_to_follow {
-            // Get the price of the currency
-            let c = &currency[..];
-            let price = crawler.get_price(c);
-            prices.insert(c, price);
-        }
-        
-        let msg = build_message(&mut prices);
-        twitter.tweet(msg);
+        // Draw the field
+        draw(&state);
 
-        std::thread::sleep(time::Duration::from_millis(1000 * config.interval_sec));
+        // Ask for user input
+        ask_user(&mut state, player);
+
+        // Check if a player won
+        if has_won(&state) {
+            draw(&state);
+            print!("Player '");
+            print_player(&player);
+            println!("' won! \\(^.^)/");
+            break;
+        }
+
+        // Check if all fields are used
+        if is_over(&state) {
+            draw(&state);
+            println!("All fields are used. No one won. (._.)");
+            break;
+        }
+
+        // Switch player
+        player = if player == 'X' { 'O' } else { 'X' }
     }
 }
